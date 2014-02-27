1dad1a20d85e9aa3eb38c22887dde7b91745d73e
pbaier01 <pbaier01@TOOLS12.dcs.bbk.ac.uk>
2014-02-27 14:36:52 +0000

    chstyle

---
+++ ISE_assignment02forPMD/src/code/CorrectlyWrongCheckStyle.java
@@ -0,0 +1,38 @@
+package code;
+
+public class CorrectlyWrongCheckStyle {
+
+	public static void MarktoGrade(int score) 
+	  {
+		if (score >= 70) 
+			{
+			System.out.println("A");
+		}
+else if (score >= 60)
+		{
+			System.out.println("B");
+	 }
+	else if (score >= 50)
+		{
+					System.out.println("C");
+	     }
+		else if (score  >=  40) {
+			    System.out.println("D");
+	}
+		else {
+	System.out.println("Fail");
+}
+	for(int i = 100;    i==0;   i--){
+		
+	}
+		
+		
+	}
+	
+
+	
+	public static void main(String[] args) {
+		MarktoGrade(88);
+	}
+
+}
--- ISE_assignment02forPMD/src/code/CorrectlyWrongPMD.java
+++ ISE_assignment02forPMD/src/code/CorrectlyWrongPMD.java
@@ -30,4 +30,16 @@ public class CorrectlyWrongPMD {
 		}else{int d = 1 +2;};
 		return duplicatesRemoved;
 	}
+	public ArrayList<Integer> checkDuplicates(){
+		int TEMP;
+		ArrayList<Integer> duplicatesRemoved = new ArrayList<Integer>();
+		TEMP = ContainsDuplicates.get(0);
+		for (int elem : ContainsDuplicates) {
+			if (elem != TEMP) {
+				duplicatesRemoved.add(elem);
+				TEMP = elem;
+			}
+		}
+		return duplicatesRemoved; 
+	}
 }
