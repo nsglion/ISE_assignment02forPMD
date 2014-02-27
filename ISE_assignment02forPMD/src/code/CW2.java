1fc3b9be2f908a300185f254964841a84b5475ab
pbaier01 <pbaier01@TOOLS12.dcs.bbk.ac.uk>
2014-02-27 13:53:47 +0000

    2 commit

---
+++ ISE_assignment02forPMD/src/code/CW2.java
@@ -0,0 +1,24 @@
+package code;
+
+public class CW2 {
+	String y = "example";
+	int x = y.length();
+
+	public String getY() {
+		return y;
+	}
+	
+	public void setY(String y) {
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+		
+	
+}
